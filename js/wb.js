bb2413e85a4d9245523e4553cd58d5c003a9cbf7
Zhang JiuKun <jiukunz@gmail.com>
2013-10-22 14:05:57 +0800

    first commit

---
+++ js/wb.js
@@ -0,0 +1,127 @@
+// An example Parse.js Backbone application based on the todo app by
+// [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses Parse to persist
+// the todo items and provide user authentication and sessions.
+
+$(function() {
+
+  Parse.$ = jQuery;
+
+  // Initialize Parse with your Parse application javascript keys
+  Parse.initialize("4CeVrA0hth3HLew7yEGXE8Ms2L003vsfc9XRGfys",
+                   "hwZbAeWsB03GDc0GjhvyVIvOh1UeXMb41XnGM0Mh");
+
+  // Todo Model
+  // ----------
+  var Wb = Parse.Object.extend("Wb");
+  var query = new Parse.Query(Wb);
+  
+
+  var GuideView = Parse.View.extend({
+    tagName: "div",
+    template: _.template($('#guide-template').html()),
+
+    initialize: function() {
+    },
+
+    render: function() {
+      $(this.el).html(this.template());
+    },
+    destroy: function() {
+      $(this.el).html("");
+    }
+  });
+
+  var WbView = Parse.View.extend({
+
+    tagName:  "div",
+
+    template: _.template($('#white-board-template').html()),
+
+    events: {
+      "click #get"       : "get",
+      "click #update"    : "update",
+    },
+
+    initialize: function() {
+      
+    },
+
+    render: function() {
+      $(this.el).html(this.template());
+      this.get();
+    },
+
+    get: function() {
+      var self = this;
+      query.equalTo("boardId", this.boardId);
+      query.find({
+        success: function(results) {
+          if(results.length != 0){
+            var wb = results[0];
+            self.$("#board-content").val(wb.get("content"));  
+          }
+        },
+        error: function(object, error) {
+        }
+      });
+    },
+
+    update: function() {
+      var self = this;
+      query.equalTo("boardId", self.boardId);
+      query.find({
+        success: function(results) {
+          if(results.length == 0){
+            var wb = new Wb();
+            wb.save({"boardId":self.boardId, "content": self.$("#board-content").val()},{
+              success: function(wb) {
+              },
+              error: function(wb, error) {
+              }
+            });
+          } else {
+            results[0].save(null, {
+              success: function(wb) {
+                wb.set("content", self.$("#board-content").val())
+                wb.save();
+              },
+              error: function(wb, error) {
+                console.log(error);
+              }
+            });
+          }
+        },
+        error: function(error) {
+        }
+      });
+    }
+  });
+
+
+  var AppRouter = Parse.Router.extend({
+    routes: {
+      ":boardId": "openHouse",
+      "": "defaultRoute"
+    },
+
+    initialize: function(options) {
+    },
+
+    openHouse: function(boardId) {
+      guideView.destroy();
+      wbView.boardId = boardId;
+      wbView.render();
+    },
+
+    defaultRoute: function(actions) {
+      guideView.render();
+    }
+  });
+
+
+  new AppRouter;
+  var guideView = new GuideView({el: $('.guide')});
+  var wbView = new WbView({el: $('.white-board')});
+
+  Parse.history.start();
+});
